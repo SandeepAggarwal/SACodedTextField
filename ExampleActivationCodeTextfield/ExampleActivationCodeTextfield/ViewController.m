7075e4ff1cdbc257cf296ad3c66a5d80f5c9d8a5
Sandeep Aggarwal <@smartSandeep1129@gmail.com>
2017-03-17 17:25:58 +0530

    example code updated

--- ExampleActivationCodeTextfield/ExampleActivationCodeTextfield/ViewController.m
+++ ExampleActivationCodeTextfield/ExampleActivationCodeTextfield/ViewController.m
@@ -10,6 +10,7 @@
 #import "ActivationCodeTextField.h"
 
 @interface ViewController ()<UITextFieldDelegate>
+@property (nonatomic, strong) UILabel* label;
 @property (nonatomic, strong) ActivationCodeTextField* textField;;
 @end
 
@@ -19,8 +20,16 @@
 {
     [super viewDidLoad];
     
+    NSInteger numDigits = 6;
+    
+    UILabel* label = [UILabel new];
+    [label setText:[NSString stringWithFormat:@"Please enter the %ld digit code below:",numDigits]];
+    [label setTextAlignment:(NSTextAlignmentCenter)];
+    self.label = label;
+    [self.view addSubview:label];
+    
     ActivationCodeTextField* textField = [ActivationCodeTextField new];
-    textField.maxCodeLength = 6;
+    textField.maxCodeLength = numDigits;
     textField.customPlaceholder = @"😀";
     textField.delegate = self;
     [textField setTextColor:[UIColor blackColor]];
@@ -33,11 +42,17 @@
 {
     [super viewDidLayoutSubviews];
     
-    CGRect frame = self.textField.frame;
+    [self.label sizeToFit];
+    CGRect frame = self.label.frame;
+    frame.origin.y = 100.0f;
+    frame.origin.x = (self.view.bounds.size.width - self.label.bounds.size.width)/2.0f;
+    self.label.frame = frame;
+    
+    frame = self.textField.frame;
     frame.size.width = [self.textField minWidthTextField];
     frame.size.height = MAX(self.textField.bounds.size.height, 44.0f);
     frame.origin.x = (self.view.bounds.size.width - frame.size.width)/2.0f;
-    frame.origin.y = 100.0f;
+    frame.origin.y = self.label.frame.origin.y + self.label.bounds.size.height + 40.0f;
     self.textField.frame = frame;
     
 }
