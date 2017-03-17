95434fbdd6df908203f69ab2fbc86a4aa1d12e32
Sandeep Aggarwal <@smartSandeep1129@gmail.com>
2017-03-17 13:42:41 +0530

    example updated

--- ExampleActivationCodeTextfield/ExampleActivationCodeTextfield/ViewController.m
+++ ExampleActivationCodeTextfield/ExampleActivationCodeTextfield/ViewController.m
@@ -7,23 +7,56 @@
 //
 
 #import "ViewController.h"
+#import "ActivationCodeTextField.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITextFieldDelegate>
+@property (nonatomic, strong) ActivationCodeTextField* textField;;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    ActivationCodeTextField* textField = [ActivationCodeTextField new];
+    textField.maxCodeLength = 6;
+    textField.customPlaceholder = @"😀";
+    textField.delegate = self;
+    [textField setTextColor:[UIColor blackColor]];
+    self.textField = textField;
+    [self.view addSubview:textField];
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    
+    CGRect frame = self.textField.frame;
+    frame.size.width = [self.textField minWidthTextField];
+    frame.size.height = MAX(self.textField.bounds.size.height, 44.0f);
+    frame.origin.x = (self.view.bounds.size.width - frame.size.width)/2.0f;
+    frame.origin.y = 100.0f;
+    self.textField.frame = frame;
+    
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [self.textField becomeFirstResponder];
+}
+
+#pragma mark - <UITextFieldDelegate>
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    NSString* acceptableCharacters = @"0123456789";
+    NSCharacterSet *cs = [[NSCharacterSet characterSetWithCharactersInString:acceptableCharacters] invertedSet];
+    
+    NSString *filtered = [[string componentsSeparatedByCharactersInSet:cs] componentsJoinedByString:@""];
+    
+    return [string isEqualToString:filtered];
+}
 
 @end
