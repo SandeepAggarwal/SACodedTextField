9282f063f1271336bd44e60005713b3728bca76b
Sandeep Aggarwal <@smartSandeep1129@gmail.com>
2017-03-25 19:10:26 +0530

    Carthage support added

---
+++ ActivationCodeTextField/Version.h
@@ -0,0 +1,19 @@
+//
+//  Version.h
+//  SACodedTextField
+//
+//  Created by Sandeep Aggarwal on 25/03/17.
+//  Copyright © 2017 Sandeep Aggarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SACodedTextField.
+FOUNDATION_EXPORT double SACodedTextFieldVersionNumber;
+
+//! Project version string for SACodedTextField.
+FOUNDATION_EXPORT const unsigned char SACodedTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SACodedTextField/PublicHeader.h>
+
+
